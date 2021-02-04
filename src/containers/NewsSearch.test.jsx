25ffd0f5da81be7968bbeedfe2f80a833202d828
Jena Boehm <boehmjena@gmail.com>
2021-02-03 18:30:39 -0800

    all tests passing but it's FAKE

--- src/containers/NewsSearch.test.jsx
+++ src/containers/NewsSearch.test.jsx
@@ -1,2 +1,13 @@
-global.fetch = require('node-fetch')
-;
+global.fetch = require('node-fetch');
+import React from 'react';
+import NewsSearch from './NewsSearch';
+import { render, screen, fireEvent } from '@testing-library/react';
+
+describe('NewsSearch container', () => {
+  it('updates the list of articles via text input', () => {
+    render(<NewsSearch />);
+
+    
+  });
+});
+
