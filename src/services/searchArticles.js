b65745b380a608d31bc22acab2f71bb8391a839b
Jena Boehm <boehmjena@gmail.com>
2021-02-03 16:51:29 -0800

    add search articles function

---
+++ src/services/searchArticles.js
@@ -0,0 +1,8 @@
+/* eslint-disable max-len */
+import fetch from 'node-fetch';
+const API_KEY = process.env.API_KEY;
+
+export const searchArticles = (text) => {
+  return fetch(`http://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}&q=${text}`)
+    .then(res => res.json());
+};
